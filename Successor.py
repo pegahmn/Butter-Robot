f60dc1cf03db65046d5c284d49d8352e64fea20e
impundit <apunditboy@gmail.com>
2022-11-17 19:41:16 +0330

    Define Environment and Agent classes

--- Successor.py
+++ Successor.py
@@ -1,96 +1,107 @@
 import numpy as np
 
 class Node:
-    state: np.ndarray(0)
-    PosR: list[int]
-    PosPs: list[list[int]]
-    PosBs: list[list[int]]
+    posR: list[int]
+    posBs: list[list[int]]
     move: str
     depth: int
     g: int
     h: float
 
-    def __init__(self, state, posR, posPs, posBs, move, depth, g, h= None) -> None:
-        self.state = state
-        self.PosR = posR
-        self.PosPs = posPs
-        self.PosBs = posBs
+    def __init__(self, posR, posBs, move, depth, g, h= None) -> None:
+        self.posR = posR
+        self.posBs = posBs
         self.move = move
         self.depth = depth
         self.g = g
         self.h = h
 
-def isInTable(pos: list, state: np.ndarray) -> bool:
-    if pos[0] < 0 or state.shape[0] <= pos[0]:
-        return False
 
-    if pos[1] < 0 or state.shape[1] <= pos[1]:
-        return False
+class Environment:
+    table: np.ndarray
+    posPs: list[list[int]]
 
-    return True
+    def __init__(self, table, posPs) -> None:
+        self.table = table
+        self.posPs = posPs
 
-def isValidMove(node: Node, move: str) -> bool:
-    up, right = {
-        'R' : (0, 1),
-        'U' : (1, 0),
-        'L' : (0, -1),
-        'D' : (-1, 0)
-    }[move]
 
-    newPos = [node.PosR[0] + up, node.PosR[1] + right]
-    state = node.state
-    
-    if not isInTable(newPos, state):
-        return False
+class Agent:
+    env: Environment
+    seen = []
 
-    if  state[newPos[0], newPos[1]] == 'x':
-        return False
-
-    if newPos in node.PosBs:
-        if newPos in node.PosPs:
+    def isInTable(self, pos: list, state: np.ndarray) -> bool:
+        if pos[0] < 0 or state.shape[0] <= pos[0]:
             return False
 
-        frontPos = [(newPos[0] if node.PosR[0]==newPos[0] else (2 * newPos[0] - node.PosR[0])),
-                (newPos[1] if node.PosR[1]==newPos[1] else (2 * newPos[1] - node.PosR[1]))]
+        if pos[1] < 0 or state.shape[1] <= pos[1]:
+            return False
 
-        if not isInTable(frontPos, state):
+        return True
+
+    def isValidMove(self, node: Node, move: str) -> bool:
+        up, right = {
+            'R' : (0, 1),
+            'U' : (1, 0),
+            'L' : (0, -1),
+            'D' : (-1, 0)
+        }[move]
+
+        newPos = [node.posR[0] + up, node.posR[1] + right]
+        state = node.state
+        
+        if not self.isInTable(newPos, state):
             return False
 
         if  state[newPos[0], newPos[1]] == 'x':
             return False
+
+        if newPos in node.posBs:
+            if newPos in self.env.PosPs:
+                return False
+
+            frontPos = [(newPos[0] if node.posR[0]==newPos[0] else (2 * newPos[0] - node.posR[0])),
+                    (newPos[1] if node.posR[1]==newPos[1] else (2 * newPos[1] - node.posR[1]))]
+
+            if not self.isInTable(frontPos, state):
+                return False
+
+            if  state[newPos[0], newPos[1]] == 'x':
+                return False
+            
+            if frontPos in node.posBs:
+                return False
+            
+        return True
+
+    def getChilde(self, node: Node, move: str) -> Node:
+        up, right = {
+            'R' : (0, 1),
+            'U' : (1, 0),
+            'L' : (0, -1),
+            'D' : (-1, 0)
+        }[move]
+
+        newPos = [node.posR[0] + up, node.posR[1] + right]
+        g = int(node.state[newPos[0], newPos[1]]) + node.g
         
-        if frontPos in node.PosBs:
-            return False
+        if newPos in node.posBs:
+            PosBs = node.posBs.copy()
+            Index = PosBs.index(newPos)
+            PosBs[Index][0] += up
+            PosBs[Index][1] += right
+        else:
+            PosBs = node.posBs
+
+        return Node(node.state, newPos, node.PosPs, PosBs, move, node.depth+1, g)
+
+    def successor(self, node: Node) -> list[Node]:
+        Childs = []
+
+        MOVES = ['R', 'U', 'L', 'D']
+        for move in MOVES:
+            if self.isValidMove(node, move) and node in self.seen:
+                Childs.append(self.getChilde(node, move))
         
-    return True
-
-def getChilde(node: Node, move: str) -> Node:
-    up, right = {
-        'R' : (0, 1),
-        'U' : (1, 0),
-        'L' : (0, -1),
-        'D' : (-1, 0)
-    }[move]
-
-    newPos = [node.PosR[0] + up, node.PosR[1] + right]
-    g = int(node.state[newPos[0], newPos[1]]) + node.g
-    
-    if newPos in node.PosBs:
-        PosBs = node.PosBs.copy()
-        Index = PosBs.index(newPos)
-        PosBs[Index][0] += up
-        PosBs[Index][1] += right
-    else:
-        PosBs = node.PosBs
-
-    return Node(node.state, newPos, node.PosPs, PosBs, move, node.depth+1, g)
-
-def successor(node: Node, seen: list[Node]) -> list[Node]:
-    Childs = []
-
-    MOVES = ['R', 'U', 'L', 'D']
-    for move in MOVES:
-        if isValidMove(node, move) and node in seen:
-            Childs.append(getChilde(node, move))
-    
-    return Childs
+        self.seen.append(node)
+        return Childs
