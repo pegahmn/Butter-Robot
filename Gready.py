e30643940fb45aa47b5dc15d4d4273e9c6c01033
impundit <apunditboy@gmail.com>
2022-11-26 23:01:41 +0330

    Add Gready

---
+++ Gready.py
@@ -0,0 +1,82 @@
+import numpy as np
+from Node import Node
+from Environment import Environment
+from Agent import Agent
+from SetCompare import Compare
+from GetBPR import GetBPRPosition
+from queue01 import Queue
+from stack01 import Stack
+
+state = np.asarray(
+    [['x', 'x', 'x', 'x', 'x'],
+    ['x', '1', '2b', '1p', 'x'],
+    ['1', '1p', '2b', '1r', '1'],
+    ['1', '1', '2', '1', '1']], dtype= np.str_
+)
+
+
+ButterPosition, RobotPosition, PointPosition=GetBPRPosition(state)
+env = Environment(state, PointPosition)
+agent = Agent(env)
+root = Node(RobotPosition[0], ButterPosition)
+
+def Gready(Root: Node,Agent: Agent,PointPosition:list):
+    stack=Stack()
+    stack.push(Root)
+    while True:
+        if stack.is_empty():
+            return None
+        state=stack.pop()
+        if Compare(state.posBs,PointPosition):
+            return state
+        childs=Agent.successor(state)
+        while childs != []:
+            maxH = childs[0].h
+            index = 0
+
+            for i in range(1, len(childs)):
+                if childs[i].h > maxH:
+                    index = i
+                    maxH = childs[i].h
+
+            stack.push(childs.pop(index))
+
+Answer=Gready(root,agent,PointPosition)
+if Answer==None:
+    print("There is no answer")
+else:
+    node = Answer
+    Moves = []
+    while node != None:
+        Moves.append(node.move)
+        node = node.parent
+
+    print("Moves: ")
+    for i in range(len(Moves)-1,-1,-1):
+        print(f"{Moves[i]} ", end='')
+
+    print(f"depth = {Answer.depth}")
+    print(f"cost = {Answer.g}")
+    for i in range(state.shape[0]):
+        print(end= "\t")
+        for j in range(state.shape[1]):
+            cell = state[i, j]
+            cell += 'p' if [i, j] in PointPosition else ''
+            cell += 'b' if [i, j] in Answer.posBs else ''
+            cell += 'r' if [i, j] == Answer.posR else ''
+            print(cell, end= "\t")
+        print()
+    print("---------------------------------------------")
+            
+
+        
+
+    
+    # ExpanedNode=Agent.successor(Root)
+    # for i in ExpanedNode:
+    #     if GoalTest(i.posBs,PointPosition):
+
+        
+
+    
+    
